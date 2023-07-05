1d952c1b145108d7b0d0ce9ea8114da9dc25e358
sudhamsh99 <92301797+sudhamsh99@users.noreply.github.com>
2023-07-05 18:25:56 +0530

    hiufd

---
+++ js/about.js
@@ -0,0 +1,10 @@
+// gsap.to(".wrapper_pent", {
+//     top: "-10%",
+//     scrollTrigger: {
+//       trigger: ".p_img_wrapper",  
+//       start: "top 70%",
+//       end: "bottom 20%",
+//       scrub: .7,
+//     //   markers: true,
+//     },
+//   });
