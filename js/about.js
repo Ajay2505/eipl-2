67787d9e5736b411177383b6ad814296bb6a4c6e
sudhamsh99 <92301797+sudhamsh99@users.noreply.github.com>
2023-07-06 13:52:49 +0530

    snjd

--- js/about.js
+++ js/about.js
@@ -1,7 +1,7 @@
 // gsap.to(".wrapper_pent", {
 //     top: "-10%",
 //     scrollTrigger: {
-//       trigger: ".p_img_wrapper",  
+//       trigger: ".p_img_wrapper",
 //       start: "top 70%",
 //       end: "bottom 20%",
 //       scrub: .7,
@@ -9,16 +9,20 @@
 //     },
 //   });
 
-var swiper = new Swiper(".mySwiper", {      
-    loop: true,
-    pagination: {
-        el: ".swiper_pagination",
-        clickable: true,
-    },
-    slidesPerView: 2,
-    spaceBetween: 40,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
+var swiper = new Swiper(".mySwiper", {
+  loop: true,
+  pagination: {
+    el: ".swiper_pagination",
+    clickable: true,
+  },
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  slidesPerView: 2,
+  spaceBetween: 40,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 });
